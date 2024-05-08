dafd4bd8195bdf7b4533f438bcc4a79e30a89947
Bruno Perdigão <brunoperdigao@tutanota.com>
2024-05-07 22:34:57 -0300

    mudança na lógica do seed

--- modulo_4/seeds/brunoperdigao-perfil.js
+++ modulo_4/seeds/brunoperdigao-perfil.js
@@ -1,11 +1,23 @@
-db = connect('mongodb://localhost/ebacpizza');
-console.log(db)
+const mongoose = require("mongoose")
 
-db.perfis.insertOne(
+const { Perfil } = require("../models")
+
+mongoose.connect('mongodb://127.0.0.1:27017/ebacpizza');
+
+const seedPerfil = [
   {
     "nomeDoUsuario": "brunoperdigao",
     "sobrenome": "Perdigão",
     "idade": 38,
     "primeiroNome": "Bruno"
   }
-)
+];
+
+const seedDB = async () => {
+  await Perfil.deleteMany({});
+  await Perfil.insertMany(seedPerfil);
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
